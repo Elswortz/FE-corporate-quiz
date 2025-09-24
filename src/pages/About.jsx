12d6cd677bc9ef60e1a41e1e2ca0d7d6a96769c3
Andrii Shkliar <elswortz@gmail.com>
2025-09-24 19:43:10 +0300

    add basic RTK configuration with test example

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,12 @@
 import { Button, Typography, Container } from '@mui/material';
 
+import TestComponent from '../components/TestComponent/TestComponent';
+
 function About() {
   return (
     <>
       <h1>Welcome to About Page</h1>
+      <TestComponent />
     </>
   );
 }
