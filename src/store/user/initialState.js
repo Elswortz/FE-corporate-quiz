097bdeb253e983e18886f9da79642a07e31d5f9f
Andrii Shkliar <elswortz@gmail.com>
2025-10-08 22:02:01 +0300

    add users redux state managment and simple pagination

--- src/store/user/initialState.js
+++ src/store/user/initialState.js
@@ -1,32 +1,10 @@
 const initialState = {
-  id: null,
-  firstName: '',
-  lastName: '',
-  email: '',
-  avatarURL: '',
+  list: [],
+  pagination: { total: 0, limit: 10, offset: 0, hasNext: false, hasPrevious: false },
+  current: null,
+  selected: null,
+  isLoading: false,
+  error: null,
 };
 
 export default initialState;
-
-// export const getUserProfile = createAsyncThunk('auth/profile', async (_, thunkAPI) => {
-//   try {
-//     const response = await userAPI.getUserProfile();
-//     return response.data;
-//   } catch (err) {
-//     return thunkAPI.rejectWithValue(err.response?.data?.message || 'Failed to load user');
-//   }
-// });
-
-// .addCase(getUserProfile.pending, state => {
-//   state.isLoading = true;
-//   state.error = null;
-// })
-// .addCase(getUserProfile.fulfilled, (state, action) => {
-//   state.user = action.payload;
-//   state.isLoading = false;
-// })
-// .addCase(getUserProfile.rejected, state => {
-//   state.isAuthenticated = false;
-//   state.user = null;
-//   state.isLoading = false;
-// }),
